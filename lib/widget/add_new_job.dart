0d2d1c86b0f771f4abf93d7bc1dd0395ffe72ac3
masterUNG <phrombutr@gmail.com>
2021-01-21 11:56:39 +0700

    update

--- lib/widget/add_new_job.dart
+++ lib/widget/add_new_job.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:ungaicapp/models/usermodel.dart';
+import 'package:ungaicapp/utility/dialog.dart';
 import 'package:ungaicapp/utility/myconstant.dart';
 import 'package:ungaicapp/utility/mystyle.dart';
 
@@ -14,6 +15,7 @@ class AddNewJob extends StatefulWidget {
 class _AddNewJobState extends State<AddNewJob> {
   List<UserModel> userModels = List();
   List<bool> chooseUsers = List();
+  String title, todo, idofficer, nameofficer;
 
   @override
   void initState() {
@@ -42,7 +44,7 @@ class _AddNewJobState extends State<AddNewJob> {
       margin: EdgeInsets.only(top: 16),
       width: 250,
       child: TextField(
-        onChanged: (value) {},
+        onChanged: (value) => title = value.trim(),
         decoration: InputDecoration(
           prefixIcon: Icon(Icons.rowing),
           hintText: 'Title:',
@@ -57,7 +59,7 @@ class _AddNewJobState extends State<AddNewJob> {
       margin: EdgeInsets.only(top: 16),
       width: 250,
       child: TextField(
-        onChanged: (value) {},
+        onChanged: (value) => todo = value.trim(),
         decoration: InputDecoration(
           prefixIcon: Icon(Icons.work),
           hintText: 'ToDo :',
@@ -71,6 +73,9 @@ class _AddNewJobState extends State<AddNewJob> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          buildIconButton(),
+        ],
         title: Text('Add New Job'),
       ),
       body: Center(
@@ -81,21 +86,38 @@ class _AddNewJobState extends State<AddNewJob> {
     );
   }
 
+  IconButton buildIconButton() => IconButton(
+        icon: Icon(Icons.cloud_upload),
+        onPressed: () {
+          if ((title?.isEmpty ?? true) || (todo?.isEmpty ?? true)) {
+            normalDialog(context, 'Have Space ? Please Fill Every Blank');
+          }
+        },
+      );
+
   Widget buildListUser() {
     return Expanded(
       child: userModels.length == 0
           ? MyStyle().showProgress()
-          : ListView.builder(
-              itemCount: userModels.length,
-              itemBuilder: (context, index) => CheckboxListTile(
-                value: chooseUsers[index],
-                subtitle: Text('Position : ${userModels[index].user}'),
-                title: Text(userModels[index].name),
-                onChanged: (value) {
-                  setState(() {
-                    chooseUsers[index] = true;
-                  });
-                },
+          : Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: ListView.builder(
+                itemCount: userModels.length,
+                itemBuilder: (context, index) => Container(
+                  decoration: BoxDecoration(
+                      color:
+                          index % 2 == 0 ? Colors.grey.shade300 : Colors.white),
+                  child: CheckboxListTile(
+                    value: chooseUsers[index],
+                    subtitle: Text('Position : ${userModels[index].user}'),
+                    title: Text(userModels[index].name),
+                    onChanged: (value) {
+                      setState(() {
+                        chooseUsers[index] = true;
+                      });
+                    },
+                  ),
+                ),
               ),
             ),
     );
